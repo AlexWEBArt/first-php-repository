9e306495a70c348fb26e33a1f08d9bff5d8667e0
AlexWEBArt <64921290+AlexWEBArt@users.noreply.github.com>
2023-07-03 16:55:13 +0600

    Update index.php
    fixed path

--- web/index.php
+++ web/index.php
@@ -5,9 +5,9 @@ declare(strict_types=1);
 session_start();
 
 if (isset($_SESSION['name'])) {
-    include '.\aboutMe.php';
-    echo '<a href=".\exit.php">Выйти</a>';
+    include './aboutMe.php';
+    echo '<a href="./exit.php">Выйти</a>';
 } else {
     echo '<p>мы пока не знаем кто вы заполните форму</p>';
-    include '.\formName.php';
+    include './formName.php';
 }
